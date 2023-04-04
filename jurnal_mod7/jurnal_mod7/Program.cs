8a8ae9822c3cc6680290e1915f17a3f18e52a04a
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2023-04-04 13:41:21 +0700

    Menambahkan class datamahasiswa dan teammemberes

--- jurnal_mod7/jurnal_mod7/Program.cs
+++ jurnal_mod7/jurnal_mod7/Program.cs
@@ -6,7 +6,41 @@ namespace jurnal_mod7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Soal ke satu
+
+            DataMahasiswa_1302194007 mhs = new DataMahasiswa_1302194007().ReadJSON();
+            // print all information from DataMahasiswa
+            Console.WriteLine("Nama    : " + mhs.firstName + " " + mhs.lastName);
+            Console.WriteLine("Age     : " + mhs.age);
+            Console.WriteLine("Gender  : " + mhs.gender);
+            Console.WriteLine("Address : " + mhs.address.streetAddress + ", " + mhs.address.city + ", " + mhs.address.state);
+            Console.WriteLine("Course  : ");
+            foreach (Course course in mhs.courses)
+            {
+                Console.WriteLine("- [" + course.code + "] " + course.name);
+            }
+
+
+            // Soal ke dua
+
+            Console.WriteLine("---------------------------------");
+            TeamMembers_1302194007 anggotaTim = new TeamMembers_1302194007().ReadJSON();
+            Console.WriteLine("Team member list: ");
+
+            // cara pertama
+            foreach (TeamDetail team in anggotaTim.members)
+            {
+                Console.WriteLine("[" + team.nim + "] " + team.firstName + " " + team.lastName + " (" + team.age +", " + team.gender +") ");
+            }
+
+            // cara kedua
+            /**for (int i = 0; i < anggotaTim.members.Count; i++)
+            {
+                Console.WriteLine("[" + anggotaTim.members[i].nim + "] " + anggotaTim.members[i].firstName + " " + anggotaTim.members[i].lastName
+                    + " (" + anggotaTim.members[i].age + ", " + anggotaTim.members[i].gender + ") ");
+            }**/
+            
         }
     }
 }
---
+++ jurnal_mod7/jurnal_mod7/TeamMembers_1302194007.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace jurnal_mod7
+{
+    public class TeamMembers_1302194007
+    {
+        public List<TeamDetail> members { get; set; }
+
+        public TeamMembers_1302194007()
+        {
+        }
+
+        public TeamMembers_1302194007 ReadJSON()
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string jsonString = File.ReadAllText(path + "/jurnal7_2_1302194007.json");
+            return JsonSerializer.Deserialize<TeamMembers_1302194007>(jsonString);
+        }
+    }
+    public class TeamDetail
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+
+        public TeamDetail() { }
+    }
+}
